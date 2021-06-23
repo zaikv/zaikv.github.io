a98f9850c82efe6af2bdf4641956e2b52a61d70f
Konstantin <z.k.v.ufa@mail.ru>
2021-06-23 16:01:07 +0300

    fix

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,22 @@ importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-messaging.js');
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCHgjVqvamE-pcy6UnarsgVespfmdDczBQ",
-    authDomain: "test-svarma.firebaseapp.com",
-    projectId: "test-svarma",
-    storageBucket: "test-svarma.appspot.com",
     messagingSenderId: "574418205961",
-    appId: "1:574418205961:web:dc5f73478e51672c00a7d8",
-    measurementId: "G-EPD0MQ4SX7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var mesaging = firebase.messaging();
+
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
